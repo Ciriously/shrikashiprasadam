a3c8c02b8bc023ad60afa508822e58d48dc2e1eb
Aditya Mishra <56506651+Ciriously@users.noreply.github.com>
2024-04-23 11:12:31 +0530

    Feat : added Breadcrumbs

--- src/Pages/SearchResults.jsx
+++ src/Pages/SearchResults.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Filter from "../components/SearchResults/Filters"
+import BreadCrumbs from '../components/ui/BreadCrumbs'
 
 const SearchResults = () => {
+
+    const crumbs = [
+        { text: 'Home', href: '#' },
+        { text: 'Rudraksha', href: '#' },
+    ];
+
     return (
-        <div>SearchResults</div>
+        <div className='bg-[#FDF7EB]'>
+            <div className="bg-white p-4 rounded-md shadow-md mb-4">
+                <BreadCrumbs crumbs={crumbs} />
+            </div>
+
+            <Filter />
+        </div>
     )
 }
 
